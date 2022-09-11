8c029d54bcd1de69eb921ef434e11566c2910505
David Walker <d0sboots@gmail.com>
2022-09-11 01:13:06 -0700

    Add error handling, select on focus

--- converter.js
+++ converter.js
@@ -6,6 +6,7 @@ const U8Array = Uint8Array;
 
 const base64_box = document.getElementById("base64");
 const json_box = document.getElementById("json");
+const error_box = document.getElementById("error-box");
 
 const wasm = U8Array.from("'''REPLACE_WASM_HERE'''", x => x.charCodeAt(0) ^ 0x6f);
 var _malloc, _do_deflate, _do_inflate, buffer, HEAPU8, HEAP32;
@@ -19,6 +20,8 @@ WebAssembly.instantiate(wasm, {}).then(out => {
   HEAP32 = new Int32Array(buffer);
   base64_box.oninput = decode;
   json_box.oninput = encode;
+  base64_box.onfocus = select_all;
+  json_box.onfocus = select_all;
 });
 
 function result2array(result_ptr) {
@@ -30,38 +33,57 @@ function result2array(result_ptr) {
   return view;
 }
 
+/** @this {HTMLTextAreaElement} */
+function select_all() {
+  this.setSelectionRange(0, this.value.length);
+}
+
+function show_error(ex) {
+  error_box.textContent = ex + "";
+}
+
 function decode() {
-  const value = base64_box.value;
-  if (value === "") {
-    // Special-case this: It's valid, but inflate will choke.
-    json_box.value = "";
-    return;
+  try {
+    const value = base64_box.value;
+    if (value === "") {
+      // Special-case this: It's valid, but inflate will choke.
+      json_box.value = "";
+      return;
+    }
+    const bin_str = atob(value);
+    const bin_buf_ptr = _malloc(bin_str.length);
+    for (var i = 0; i < bin_str.length; ++i) {
+      HEAPU8[bin_buf_ptr+i] = bin_str.charCodeAt(i);
+    }
+    // result_ptr points to a struct which is a 32-bit length followed by that much data
+    // The data has already been "freed", but it will still be here to be copied
+    const result_ptr = _do_inflate(bin_buf_ptr, bin_str.length);
+    const utf8_buf = result2array(result_ptr);
+    const out_value = new TextDecoder("utf-8", {fatal: true}).decode(utf8_buf);
+    json_box.value = out_value;
+    show_error("");
+  } catch (ex) {
+    show_error(ex);
   }
-  const bin_str = atob(value);
-  const bin_buf_ptr = _malloc(bin_str.length);
-  for (var i = 0; i < bin_str.length; ++i) {
-    HEAPU8[bin_buf_ptr+i] = bin_str.charCodeAt(i);
-  }
-  // result_ptr points to a struct which is a 32-bit length followed by that much data
-  // The data has already been "freed", but it will still be here to be copied
-  const result_ptr = _do_inflate(bin_buf_ptr, bin_str.length);
-  const utf8_buf = result2array(result_ptr);
-  const out_value = new TextDecoder("utf-8", {fatal: true}).decode(utf8_buf);
-  json_box.value = out_value;
 }
 
 function encode() {
-  const value = json_box.value;
-  const utf8_buf_ptr = _malloc(value.length * 2);
-  const arr = new U8Array(buffer, utf8_buf_ptr, value.length * 2);
-  const encode_result = new TextEncoder().encodeInto(value, arr);
-  if (encode_result.read < value.length) {
-    throw new Error("Couldn't encode string as UTF-8");
+  try {
+    const value = json_box.value;
+    const utf8_buf_ptr = _malloc(value.length * 2);
+    const arr = new U8Array(buffer, utf8_buf_ptr, value.length * 2);
+    const encode_result = new TextEncoder().encodeInto(value, arr);
+    if (encode_result.read < value.length) {
+      throw new Error("Couldn't encode string as UTF-8");
+    }
+    // result_ptr points to a struct which is a 32-bit length followed by that much data
+    // The data has already been "freed", but it will still be here to be copied
+    const result_ptr = _do_deflate(utf8_buf_ptr, encode_result.written);
+    const bin_str = fromCharCode(...result2array(result_ptr));
+    const base_str = btoa(bin_str);
+    base64_box.value = base_str;
+    show_error("");
+  } catch (ex) {
+    show_error(ex);
   }
-  // result_ptr points to a struct which is a 32-bit length followed by that much data
-  // The data has already been "freed", but it will still be here to be copied
-  const result_ptr = _do_deflate(utf8_buf_ptr, encode_result.written);
-  const bin_str = fromCharCode(...result2array(result_ptr));
-  const base_str = btoa(bin_str);
-  base64_box.value = base_str;
 }
